d4b24fe46f52316d74eddaa7e92a3b0ace8b08c4
Murodbek699 <murodbek.0338@gmail.com>
2022-07-29 11:11:33 +0500

    number bug fix

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -19,7 +19,7 @@ class SiteController extends Controller
 
        $data = $request -> validate([
             'name' => 'required|min:3',
-            'phone' => 'required|numeric|min:9|max:9',
+            'phone' => 'required|integer|digits_between:0,9',
             'message' => 'required|max:2048'
         ]);
         //Formatting
